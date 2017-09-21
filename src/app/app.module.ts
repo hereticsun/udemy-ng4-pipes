e9204a2657701387b33c91b4377ef5944e9237ee
Azlan Cuttilan <azlan@bynd.com>
2017-09-21 09:20:55 +0100

    226: Creating a custom pipe
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { ShortenPipe } from './shorten.pipe';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortenPipe
   ],
   imports: [
     BrowserModule,
