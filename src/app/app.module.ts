6b1dcddc7b94e7d933450c82eb8652ebb6c235f8
Azlan Cuttilan <azlan@bynd.com>
2017-09-22 08:11:57 +0100

    228: Creating a filter pipe
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 import { ShortenPipe } from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShortenPipe
+    ShortenPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
